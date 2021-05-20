2195c9652fb7b5dac4f4e8d5a71208e435cace32
Thanh Tung Truong <tungttse@gmail.com>
2021-05-20 16:29:06 -0500

    Add Nhan solution

--- leecode/MaximumSubarray.java
+++ leecode/MaximumSubarray.java
@@ -4,6 +4,64 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/*
+*
+*
+* # class Integer
+#   N_BYTES = [42].pack('i').size
+#   N_BITS = N_BYTES * 16
+#   MAX = 2 ** (N_BITS - 2) - 1 # 4611686018427387903
+#   MIN = -MAX - 1
+# end
+
+def n_lowest_integers(k, n, sum)
+  result = []
+  return result if k < 0 || n < 0 || sum < 0 || k > sum
+
+  # k == sum
+  if(k == sum)
+    result << k
+    temp = k
+    while(n > 1) do
+      temp *= 10
+      n -= 1
+      result << temp
+    end
+    return result
+  end
+
+  # k < sum
+  minimum_digits = 0
+  sub_sum = sum - k
+  temp = 0
+  while(sub_sum > 10) do
+    sub_sum -= 9
+    temp += 9*(10**minimum_digits)
+    minimum_digits += 1
+  end
+  # swap sub_sum and k
+  sub_sum,k = k,sub_sum if sub_sum > k
+  minimum_number = sub_sum*10**(minimum_digits + 1) + k*10**(minimum_digits)
+  minimum_number += temp if minimum_digits > 0
+  # MAX Integer is 4611686018427387903
+  for i in minimum_number..4611686018427387903 do
+    digits = i.digits
+    next if !digits.include?(k)  # Check whether this integer include k
+    next if digits.sum != sum # Check whether sum all digits is sum
+
+    result << i
+    break if result.length == n # break when get enough n integers
+  end
+
+  return result
+end
+
+p n_lowest_integers(7, 10, 10)
+
+*
+*
+* */
+
 public class MaximumSubarray {
     public static int maxSubArray(int[] nums) {
 //        int maxSoFar = nums[0];
