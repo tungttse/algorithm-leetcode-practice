48f6458c719ca5715d2208c6edbc34da5bddf0c0
TungTop <tungttse@gmail.com>
2024-12-21 11:28:20 -0600

    1493

---
+++ 1493.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    let count0 = 0;
+    let left = 0;
+    let i = 0;
+    for(i = 0; i < nums.length; i++) {
+        if(nums[i] == 0) {
+            count0++;
+        }
+
+        if(count0 > 1) {
+            if (nums[left++] === 0) {
+                count0--;
+            }
+        }
+    }
+
+    return i - left - 1;
+};
+
+let nums = [0,1,1,1,0,1,1,0,1];
+console.log(longestSubarray(nums));
